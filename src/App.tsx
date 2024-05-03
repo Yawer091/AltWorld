6c3c3d07751706074cc4841b5cc32fc67e252d44
Yawer091 <yaweratique90@gmail.com>
2024-05-03 15:04:10 +0530

    menu

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,24 @@ import Middle from "./components/Middle";
 import Side from "./components/Side";
 // import Vcard from "./components/Vcard";
 
-const data = [
-  {
-    name: "About",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, officia inventore repudiandae ",
-  },
-  {
-    name: "Experience",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, officia inventore repudiandae ",
-  },
-  {
-    name: "Hobbies",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, officia inventore repudiandae ",
-  },
-  {
-    name: "Introduction",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, officia inventore repudiandae ",
-  },
-];
+// const data = [
+//   {
+//     name: "About",
+//     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, officia inventore repudiandae ",
+//   },
+//   {
+//     name: "Experience",
+//     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, officia inventore repudiandae ",
+//   },
+//   {
+//     name: "Hobbies",
+//     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, officia inventore repudiandae ",
+//   },
+//   {
+//     name: "Introduction",
+//     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, officia inventore repudiandae ",
+//   },
+// ];
 function App() {
   return (
     <div className=" flex bg-slate-100 h-[670px] gap-30px">

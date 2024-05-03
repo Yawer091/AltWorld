12fa91f2e265627fe341411a5c0d307ea8e06db1
Yawer091 <yaweratique90@gmail.com>
2024-05-03 23:29:57 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,10 @@ import "./App.css";
 import Modal from "./components/Modal";
 import Sidebar from "./components/Side";
 
-// import Vcard from "./components/Vcard";
-
-// const data = [
-//   {
-//     name: "About",
-//     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, officia inventore repudiandae ",
-//   },
-//   {
-//     name: "Experience",
-//     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, officia inventore repudiandae ",
-//   },
-//   {
-//     name: "Hobbies",
-//     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, officia inventore repudiandae ",
-//   },
-//   {
-//     name: "Introduction",
-//     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, officia inventore repudiandae ",
-//   },
-// ];
 function App() {
   return (
     <>
-      <div className="flex mt-[10px] ml-[30px]  h-[900px]">
+      <div className=" flex mt-[10px] ml-[30px]  h-[900px]">
         <Sidebar />
         <Modal />
       </div>
